84d09bbdcb3859286585f8ab4752f3577cfc7c8c
note trabalho <jonathanlemos@outlook.com>
2018-07-23 16:27:46 -0300

    contatos

---
+++ Contatos/src/app/servicos/contatos-data-base.service.ts
@@ -0,0 +1,23 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { ContatoModel } from "../modelos/contato-model";
+
+@Injectable()
+export class ContatosDataBaseService{
+
+    meusContatos: ContatoModel[] = [];
+    enviarContato = new EventEmitter();
+
+    constructor (){ }
+
+    setContato(novoContato: ContatoModel): void {
+        this.meusContatos.push(novoContato);
+        this.enviarContato.emit(this.meusContatos);
+    }
+
+    getContato(id:number): ContatoModel {
+        let contato: ContatoModel;
+        contato = this.meusContatos[id];
+        return contato;
+    }
+
+}
